03ff87d68745c5a77ad2373e6842e4a2fca0fa53
Asep Cahyudin <savefaaizuun@gmail.com>
2020-03-13 17:09:43 +0700

    cek controller

--- controllers/default.go
+++ controllers/default.go
@@ -9,7 +9,7 @@ type MainController struct {
 }
 
 func (c *MainController) Get() {
-	c.Data["Website"] = "beego.me"
-	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["Website"] = "Testing Golang"
+	c.Data["Email"] = "savefaaizuun@gmail.com"
 	c.TplName = "index.tpl"
 }

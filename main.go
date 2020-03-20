fef1c3a7803e32fae6d6a742f20b098cb6bc82da
Asep Cahyudin <savefaaizuun@gmail.com>
2020-03-20 16:28:20 +0700

    test connect

--- main.go
+++ main.go
@@ -3,9 +3,20 @@ package main
 import (
 	_ "testing-golang/routers"
 	"github.com/astaxie/beego"
+	"fmt"
+    "github.com/astaxie/beego/orm"
+    _ "github.com/go-sql-driver/mysql"
 )
 
+func init() {
+    orm.RegisterDriver("mysql", orm.DRMySQL)
+
+    orm.RegisterDataBase("default", "mysql", "root:root@/perpusku_gc?charset=utf8")
+}
+
 func main() {
+	o := orm.NewOrm()
+    o.Using("default") // Using default, you can use other database
 	beego.Run()
 }
 
